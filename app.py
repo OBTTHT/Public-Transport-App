501683477f338bedc729d58cbb4f2419cd154a59
OBTTHT <95370738+OBTTHT@users.noreply.github.com>
2023-01-30 21:42:16 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,97 @@
+# Başlangıç
+
+import time
+
+while True:
+
+    soru_1 = print("""
+    Toplu Taşıma Uygulamamıza Hoş Geldiniz!
+
+    1) Yeni Kullanıcı Tanımla
+    2) Kullanıcı Olarak Tanımlıyım
+    """)
+
+    secim = int(input("Lütfen Yapmak İstediğiniz İşlemin Numarasını Girin: "))
+    if (secim == 1):
+       name = str(input("Lütfen İsminizi Girin: "))
+       surname = str(input("Lütfen Soyadınızı Girin: "))
+       age = int(input("Lütfen Yaşınızı Girin: "))
+       balance = int(input("Bakiye Girin: "))
+       password = input("Lütfen Şifrenizi Belirleyin: ")
+       block = 3
+       Login = False
+       user_info = name,surname,age,balance,password
+       print(soru_1)
+
+    if (secim == 2):
+        if (Login == False):
+            girilen_sifre = input("Lütfen Şifrenizi Girin: ")
+        if (girilen_sifre == password):
+            Login = True       
+            print("""
+            Aşağıdaki Seçeneklerden Birini Seçin;
+
+            1) Bakiye Sorgula 
+            2) Para Yatır
+            3) Servis Ücreti Al
+            4) Çıkış
+            """)
+                
+            secim2 = int(input("Lütfen Yapmak İstediğiniz İşlemin Numarasını Girin: "))
+            if (secim2 == 1):
+                print(f"""
+                Kullanıcı Bilgileri:
+
+                İsim: {name} 
+                Soyisim: {surname}
+                Yaş: {age}
+                Bakiye: {balance}
+                """)
+            if (secim2 == 2):
+                para_yatir = int(input("Lütfen Yatırmak İstediğiniz Para Miktarını Girin: "))
+                balance += para_yatir
+            if (secim2 == 3):
+                if (age < 18):
+                    if (balance >= 3):    
+                        print("Öğrenci...")
+                        balance -= 3
+                        print(f"""
+                        Kesilen Miktar: 3 TL
+                        Kalan Bakiye: {balance}
+                        """)
+                    else:
+                        print("Yetersiz Bakiye")
+                        break  
+                if (age >= 18 and age < 55):
+                    if (balance >= 5): 
+                       print("Tam...")
+                       balance -= 5
+                       print(f"""
+                       Kesilen Miktar: 5 TL
+                       Kalan Bakiye: {balance}
+                       """)
+                    else:
+                        print("Yetersiz Bakiye")
+                        break
+                if (age >= 55):
+                    if (balance >= 2):
+                       print("Yaşlı...")
+                       balance -= 2
+                       print(f"""
+                       Kesilen Miktar: 2 TL
+                       Kalan Bakiye: {balance}
+                       """)
+                    else:
+                        print("Yetersiz Bakiye")
+                        break   
+            if (secim2 == 4):
+                print("Çıkış Yapılıyor...")
+                time.sleep(1)
+                Login = False
+                break      
+        else:
+            print("Yanlış Parola!")
+            block -= 1
+            if (block == 0):
+                print("Çok Fazla Yanlış Giriş Denemesi Yaptığınız İçin Bloklandınız, Yetkililer İle İletişime Geçin.")
+                break
